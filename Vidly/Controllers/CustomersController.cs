7db2169ad94df52bea26f50321fdc51e54641f5d
Koshiz <koshilasandaru2@gmail.com>
2023-12-28 22:46:56 +0530

    Added CustomerController

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomer();
+            return View(customers);
+        }
+
+        private IEnumerable<Customer> GetCustomer()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith"},
+                new Customer { Id = 2, Name = "Mary Williams"}
+            };
+        }
+    }
+}
